0aa69cee29c69d46ffcce940b9ce6367d3d4e3eb
pprun <pprun@pprun-hp>
2015-10-16 22:01:12 +0800

    use mysql

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
@@ -17,7 +17,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Test class for the UserResource REST controller.
